e8b5dd2729643f3e5baec7201e13739e3dc20b45
”Tolik1221544” <chervinskiy_anatoliy@mail.ru>
2022-05-14 16:01:26 +0300

    initial commit

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private int x, y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x += 1;
+            y = (int)(25 * Math.Sin(180 * x / Math.PI));
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawEllipse(new Pen(Color.Black, 1), 3 * x + 50, y + 50, 50, 50);
+        }
+    }
+}
